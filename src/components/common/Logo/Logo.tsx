c50ed163cf4c17ab45dd77c31eca9cdf2c9e1b9a
Alexander Rozhkov <rozhkov2222@icloud.com>
2026-01-23 22:11:52 +1000

    hotfix: Апскейл логотипа

--- src/components/common/Logo/Logo.tsx
+++ src/components/common/Logo/Logo.tsx
@@ -1,6 +1,6 @@
 import {FC, JSX} from 'react';
-import imageLogo from '@/assets/images/logo.png'
-import imageFooterLogo from '@/assets/images/footer-logo.webp'
+import imageLogo from '@/assets/images/logo.svg'
+import imageFooterLogo from '@/assets/images/footer-logo.svg'
 import classNames from 'classnames';
 import styles from './Logo.module.scss';
 import {LogoType} from '@/enums/LogoTypeEnum';
@@ -8,28 +8,28 @@ import Image from 'next/image';
 
 /** Интерфейс пропсов компонента. */
 interface IProps {
-    /** Класс корневого блока. */
-    className?: string;
+	/** Класс корневого блока. */
+	className?: string;
 
-    /** Тип логотипа. */
-    type?: LogoType;
+	/** Тип логотипа. */
+	type?: LogoType;
 }
 
 /** Логотип сайта. */
 const Logo: FC<IProps> = (
 {
-    className,
-    type = LogoType.HEADER,
+	className,
+	type = LogoType.HEADER,
 }): JSX.Element => {
-    return (
-        <a href="/">
-            <Image
-            src={LogoType.HEADER === type ? imageLogo : imageFooterLogo}
-            alt="Логотип компании Уютный Дом"
-            className={classNames(styles.logo, className)}
-            />
-        </a>
-    );
+	return (
+	<a href="/">
+		<Image
+		src={LogoType.HEADER === type ? imageLogo : imageFooterLogo}
+		alt="Логотип компании Уютный Дом"
+		className={classNames(LogoType.HEADER === type ? styles.logo : styles.footerLogo, className)}
+		/>
+	</a>
+	);
 }
 
 export default Logo;
